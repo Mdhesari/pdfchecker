ca8bd1eaad40c8270a740666ee2adbd15e9b3ab8
Mohamad Hesari <mdhesari99@gmail.com>
2025-12-24 16:24:22 +0330

    chore: version to v0.1.5

--- pdf.go
+++ pdf.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version is the current semantic version of the pdfchecker package.
-const Version = "0.1.5"
+const Version = "0.1.6"
 
 var (
 	ErrMaliciousPDF         = errors.New("PDF contains potentially malicious content")
